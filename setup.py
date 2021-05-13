bff433787d4e378813097a5c02cd9e26c445e517
Michael Katz <mikekatz04@gmail.com>
2021-05-12 20:38:16 -0500

    added very basic setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+# from future.utils import iteritems
+from setuptools import setup
+
+setup(
+    name="Eryn",
+    # Random metadata. there's more you can supply
+    author="Michael Katz, Nikos Karnesis",
+    version="0.1",
+    packages=[
+        "eryn",
+        "eryn.backends",
+        "eryn.moves",
+        # "eryn.pipeline",
+        # "eryn.utils",
+    ],
+)
