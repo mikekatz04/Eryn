123d1bd539ba49efb0055fe090a01f0eed431f6d
mikekatz04 <mikekatz04@gmail.com>
2025-09-18 16:32:50 -0700

    progress towards new install structure #4 [ci:build-wheels] [ci:build-pages] [doc:run-notebooks]

--- src/eryn/__init__.py
+++ src/eryn/__init__.py
@@ -1,1 +1,35 @@
-from eryn._version import __version__
+"""GPU Backend Tools."""
+
+# ruff: noqa: E402
+try:
+    from eryn._version import (  # pylint: disable=E0401,E0611
+        __version__,
+        __version_tuple__,
+    )
+
+except ModuleNotFoundError:
+    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+
+    try:
+        __version__ = version(__name__)
+        __version_tuple__ = tuple(__version__.split("."))
+    except PackageNotFoundError:  # pragma: no cover
+        __version__ = "unknown"
+        __version_tuple__ = (0, 0, 0, "unknown")
+    finally:
+        del version, PackageNotFoundError
+
+_is_editable: bool
+try:
+    from . import _editable
+
+    _is_editable = True
+    del _editable
+except (ModuleNotFoundError, ImportError):
+    _is_editable = False
+
+__all__ = [
+    "__version__",
+    "__version_tuple__",
+    "_is_editable",
+]
