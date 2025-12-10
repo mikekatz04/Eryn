b0834dd90972f6f962c6b3ee2d1af530347bdae4
Alessandro Santini <alesandro.santini@aei.mpg.de>
2025-12-10 15:45:39 +0100

    introduce updatestep and add automatic plotting diagnostic

--- src/eryn/utils/updates.py
+++ src/eryn/utils/updates.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from abc import ABC
+import dataclasses
 
 import numpy as np
 
@@ -20,6 +21,103 @@ class Update(ABC, object):
         """
         raise NotImplementedError
 
+class CompositeUpdate(Update):
+    """A composite update that chains multiple Update objects together."""
+    
+    def __init__(self, updates: list):
+        """
+        Args:
+            updates (list): List of Update objects to chain together.
+        """
+        self._updates = updates
+    
+    def __call__(self, iter, last_sample, sampler):
+        """Call all chained updates in sequence."""
+        for update in self._updates:
+            update(iter, last_sample, sampler)
+    
+    def __add__(self, other):
+        """Concatenate with another Update or CompositeUpdate."""
+        if isinstance(other, CompositeUpdate):
+            return CompositeUpdate(self._updates + other._updates)
+        elif isinstance(other, Update):
+            return CompositeUpdate(self._updates + [other])
+        else:
+            return NotImplemented
+    
+    def __radd__(self, other):
+        """Support other + self."""
+        if isinstance(other, CompositeUpdate):
+            return CompositeUpdate(other._updates + self._updates)
+        elif isinstance(other, Update):
+            return CompositeUpdate([other] + self._updates)
+        else:
+            return NotImplemented
+    
+    def __repr__(self):
+        return f"CompositeUpdate({self._updates})"
+
+
+@dataclasses.dataclass
+class UpdateStep(Update):
+    """Base class for chainable update steps."""
+    nsteps: int = 100
+    increment: int = 1
+    increment_every: int = 500
+    stop: int = None
+    
+    def __add__(self, other):
+        """Concatenate with another Update or CompositeUpdate."""
+        if isinstance(other, CompositeUpdate):
+            return CompositeUpdate([self] + other._updates)
+        elif isinstance(other, Update):
+            return CompositeUpdate([self, other])
+        else:
+            return NotImplemented
+    
+    def __radd__(self, other):
+        """Support other + self."""
+        if isinstance(other, CompositeUpdate):
+            return CompositeUpdate(other._updates + [self])
+        elif isinstance(other, Update):
+            return CompositeUpdate([other, self])
+        else:
+            return NotImplemented
+    
+    def check_step(self, iteration):
+        """Check if the update should be applied at this iteration.
+        
+        The diagnostic frequency decreases over time. The interval between
+        diagnostics is multiplied by `increment` every `increment_every` steps.
+        
+        Example with nsteps=100, increment=2, increment_every=500:
+            - iterations 0-499: check every 100 steps (but not at 0)
+            - iterations 500-999: check every 200 steps
+            - iterations 1000-1499: check every 400 steps
+            - etc.
+        """
+        if iteration == 0:
+            return False
+        
+        exponent = iteration // self.increment_every
+        interval = self.nsteps * (self.increment ** exponent)
+        
+        if self.stop is not None and iteration >= self.stop:
+            return False
+        
+        return (iteration % interval == 0)
+
+    def update(self, iteration, last_sample, sampler):
+        """Override this method in subclasses to define the update behavior."""
+        raise NotImplementedError("Subclasses must implement update() method.")
+    
+    def __call__(self, iteration, last_sample, sampler):
+        """Call the update if the step condition is met."""
+        if self.check_step(iteration):
+            print(f'Calling {self.__class__.__name__} at iteration {iteration}')
+            self.update(iteration, last_sample, sampler)
+        
+   
 
 class AdjustStretchProposalScale(Update):
     def __init__(
